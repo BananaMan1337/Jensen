99d7cb924dc61c4ba0be9092adeb881882e2e2b1
BananaMan1337 <Samuelbjorkkhan@gmail.com>
2024-03-13 10:37:08 +0100

    Klar

---
+++ Lektion1.py
@@ -0,0 +1,14 @@
+S= 'Bananer i pyjamas!'
+print(S)
+
+print(1+2)
+print(2-1)
+print(4*2)
+print(9/2)
+
+print("Du får så här många bananer")
+print(10-2+5/2)
+
+print("Du får", 30-10+5+2, "bananer, WOW!")
+
+print("Bananer:\nPython\tJava\nJavaScript")
