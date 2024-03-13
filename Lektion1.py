4408a85bdf6515feeb16afdb09376726cc212002
BananaMan1337 <Samuelbjorkkhan@gmail.com>
2024-03-13 10:37:30 +0100

    ?

--- Lektion1.py
+++ Lektion1.py
@@ -12,3 +12,31 @@ print(10-2+5/2)
 print("Du får", 30-10+5+2, "bananer, WOW!")
 
 print("Bananer:\nPython\tJava\nJavaScript")
+
+print(12-7)
+print(24/3+9)
+print(8*2 * (5 - 3) + 16/4 -1)
+print("Hej!\nHur mår du?")
+print("1"+"1")
+print(3*"Banana")
+print("banan"+"banan")
+print("banan","banan")
+print(20*"Jensen")
+print("jensen\n" *20)
+print("jensen\t" *20)
+print("hej\t"*10)
+
+print("Kör 70km/h i 1.5 timmar hur långt?\n", 70*1.5,"km")
+
+
+
+
+
+
+
+Question = input("What is 5-2")
+
+if Question=="3":
+    print("Correct")
+else:
+    print("incorrect")
