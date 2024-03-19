ad9876bcd272a5791ffc3c8efe570b28caeee58c
BananaMan1337 <Samuelbjorkkhan@gmail.com>
2024-03-19 09:19:48 +0100

    Lektion 3 mid lesson changes

---
+++ Kladd/Kladd to paste.py
@@ -0,0 +1,2 @@
+print (round (1337.1337,2))
+print (round (1337.1337))
---
+++ Lektioner/Lektion3.py
@@ -0,0 +1,19 @@
+name = input("What is your name?")
+age = int(input("Hur gammal är du?"))
+height = float(input("Hur lång är du i cm?"))
+
+print("Hej", name,"du är",age,"år gammal och",height,"cm lång")
+
+ordtal=input("Input number")
+print(5*ordtal)
+
+
+tal= int(input ("Input number"))
+print(5 * tal)
+
+fee= 358.3
+namn=input("Hej! Välkommen till BananaFitness, vad är ditt namn?")
+hours =int(input("Hur många timmar i veckan tänkte du att träna med oss?"))
+print("Vad kul", namn+"!", "För att träna", hours, "timmar i veckan så skulle det kosta", hours*fee,"kr per vecka")
+print (round (1337.1337,2))
+print (round (1337.1337))
