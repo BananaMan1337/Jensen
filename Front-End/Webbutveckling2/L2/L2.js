77b118f4288c9cfe28220e409467b3462e7e8a3d
BananaMan1337 <Samuelbjorkkhan@gmail.com>
2024-06-10 11:27:29 +0200

    lesson 2 classroom done, some javascript and such.

---
+++ Front-End/Webbutveckling2/L2/L2.js
@@ -0,0 +1,21 @@
+let name1 = "Sam";
+let age = 23;
+let active = true;
+let price = 123.500001;
+
+console.log("name:", name1, typeof name1)
+console.log("age:", age, typeof age)
+console.log("active:", active, typeof active)
+console.log("price:", price, typeof price)
+
+let sum = age + price
+let product = age * 2
+
+console.log("Sum:", sum)
+console.log("product:",product)
+
+let stringage = age.toString();
+let shortprice = price.toFixed(2);
+
+console.log("Stringage", age)
+console.log("shortprice", price)
